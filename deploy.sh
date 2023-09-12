c9b9fc242d1ed9f2a0b915d99335a3155121df7f
voskresenskiy.v <vladislav.voskresenskiy@gmail.com>
2023-09-12 16:38:52 +0300

    style

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:SiRiuS-040/test_blog_vue.git master:gh-pages
+git push -f git@github.com:SiRiuS-040/test_shop-2_vue.git master:gh-pages
 
 cd -
